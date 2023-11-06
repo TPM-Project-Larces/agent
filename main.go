ac40a9dae8c38e4da1080e45adedbcc7fa2c0e90
Douglas Araujo <douglas.araujo@aluno.uece.br>
2023-11-06 12:48:23 -0300

    feat(decryption): add file decryption

--- main.go
+++ main.go
@@ -1,10 +1,18 @@
 package main
 
 import (
-	"github.com/TPM-Project-Larces/back-end.git/router"
+	"github.com/TPM-Project-Larces/agent.git/router"
 )
 
+// @title Agent API
+// @description Agent Operations
+// @version 1.0.0
+// @contact {
+//   name: "Computer Networks and Security Laboratory (LARCES)",
+//   url: "https://larces.uece.br/",
+//   email: "larces@uece.br
+// }
+// @BasePath /
 func main() {
-
-	router.Initialize()
+	router.InitializeRoutes()
 }
